422e6ce7e622ed3b0d748672ede00991f21f4ec4
Rutvik Parmar <parmarrutvik1807@gmail.com>
2025-09-04 13:18:26 +0530

    change auth pages

--- src/pages/authPages/Login.jsx
+++ src/pages/authPages/Login.jsx
@@ -6,22 +6,22 @@ import { Link } from 'react-router'
 const Login = () => {
     return (
         <div className='w-full h-screen flex items-center justify-center bg-gradient-to-br from-white via-sky-100 to-sky-200'>
-            <div className='max-w-[80vw] h-[89vh] flex flex-col items-center justify-center bg-gradient-to-br from-white via-sky-100 to-sky-200 shadow-2xl p-5 rounded-md border-1'>
-                <div className='flex items-center justify-center rounded-full bg-sky-900 shadow-lg p-3'>
+            <div className='max-w-[80vw] flex flex-col items-center justify-center bg-gradient-to-br from-white via-sky-100 to-sky-200 shadow-2xl p-2 rounded-md border-1'>
+                <div className='flex items-center justify-center rounded-full bg-sky-900 shadow-lg p-3 mt-3'>
                     <LogIn size={30} />
                 </div>
-                <h1 className='text-2xl font-bold py-2 '>Welcome Back!</h1>
-                <p className='max-w-[80vw] text-center px-5 py-2'>Login to your account to continue shopping and track your orders.</p>
-                <div className='w-full flex flex-col items-center justify-center gap-2 py-8'>
+                <h1 className='sm:text-2xl text-lg font-bold py-2'>Welcome Back!</h1>
+                <p className='max-w-[70vw] text-center px-5 py-2 sm:text-sm text-xs'>Login to your account to continue shopping and track your orders.</p>
+                <div className='w-full flex flex-col items-center justify-center gap-2 py-5'>
                     <div className=''>
-                        <input type="text" placeholder='email' className='bg-sky-50 px-5 py-3 rounded-sm' />
+                        <input type="text" placeholder='email' className='bg-sky-50 px-5 py-3 rounded-sm sm:text-sm text-xs' />
                     </div>
                     <div>
-                        <input type="text" placeholder='password' className='w-full bg-sky-50 px-5 py-3 rounded-sm' />
+                        <input type="text" placeholder='password' className='w-full bg-sky-50 px-5 py-3 rounded-sm sm:text-sm text-xs' />
                     </div>
 
-                    <Button className="bg-sky-800 hover:bg-sky-800 px-18 py-6 mt-5">LOG IN</Button>
-                    <p className='text-center'>Don't have an account?.<Link className='text-sky-950' to='/register'>Register</Link></p>
+                    <Button className="bg-sky-800 hover:bg-sky-800 px-18 sm:py-6 mt-5 sm:text-sm text-xs">LOG IN</Button>
+                    <p className='text-center sm:text-sm text-xs'>Don't have an account?.<Link className='text-sky-950' to='/register'>Register</Link></p>
                 </div>
             </div>
         </div>
--- src/pages/authPages/Register.jsx
+++ src/pages/authPages/Register.jsx
@@ -6,18 +6,20 @@ import { Link } from 'react-router'
 const Register = () => {
     return (
         <div className='w-full h-screen flex items-center justify-center bg-gradient-to-br from-white via-sky-100 to-sky-200'>
-            <div className='max-w-[80vw] h-[89vh] flex flex-col items-center justify-center bg-gradient-to-br from-white via-sky-100 to-sky-200 shadow-2xl p-5 rounded-md border-1'>
-                <div className='flex items-center justify-center rounded-full bg-sky-900 shadow-lg p-3'>
+            <div className='max-w-[80vw] flex flex-col items-center justify-center bg-gradient-to-br from-white via-sky-100 to-sky-200 shadow-2xl p-5 rounded-md border-1'>
+                <div className='flex items-center justify-center rounded-full bg-sky-900 shadow-lg p-3 mt-3'>
                     <UserRoundPlus size={30} />
                 </div>
-                <h1 className='text-2xl font-bold py-2 text-center'>Create Your Account</h1>
-                <p className='max-w-[80vw] text-center px-5 py-2'>Join us today and enjoy a seamless shopping experience.</p>
-                <div className=' flex flex-col items-center justify-center gap-2 py-8'>
-                    <input type="text" placeholder='email' className='bg-sky-50 px-5 py-3 rounded-sm' />
-                    <input type="text" placeholder='password' className='bg-sky-50 px-5 py-3 rounded-sm' />
+                <h1 className='sm:text-2xl text-lg font-bold py-2 text-center'>Create Your Account</h1>
+                <p className='max-w-[80vw] text-center px-5 py-2 sm:text-sm text-xs'>Join us today and enjoy a seamless shopping experience.</p>
+                <div className=' flex flex-col items-center justify-center gap-2 py-5'>
+                    <input type="text" placeholder='email' className='bg-sky-50 px-5 py-3 rounded-sm sm:text-sm text-xs' />
+                    <input type="text" placeholder='password' className='bg-sky-50 px-5 py-3 rounded-sm sm:text-sm text-xs' />
+                    <input type="text" placeholder='email' className='bg-sky-50 px-5 py-3 rounded-sm sm:text-sm text-xs' />
+                    <input type="text" placeholder='password' className='bg-sky-50 px-5 py-3 rounded-sm sm:text-sm text-xs' />
 
-                    <Button className="bg-sky-800 hover:bg-sky-800 px-12 py-6 mt-5">REGISTER</Button>
-                    <p>allready have an account?.<Link className='text-sky-950' to='/login'>Log in</Link></p>
+                    <Button className="bg-sky-800 hover:bg-sky-800 px-12 sm:py-6 mt-5 sm:text-sm text-xs">REGISTER</Button>
+                    <p className='sm:text-sm text-xs'>allready have an account?.<Link className='text-sky-950' to='/login'>Log in</Link></p>
                 </div>
             </div>
         </div>
